421c99c3adf38d9ef1b95f8ec62cbff5bca29c50
StianSka <stian.skatvedt@gmail.com>
2023-08-02 12:27:46 +0200

    XD

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,44 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var inmemoryDb = new List<int>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    1,2,3,4,5
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/api/test", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return inmemoryDb;
+});
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export default function Test() {
+  const [numbers, setNumbers] = useState([]);
+
+  /*
+  useEffect(() => {
+    axios
+      .get("/api/test")
+      .then((res) => {
+        setNumbers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  */
+  useEffect(() => {
+    const getNums = async () => {
+      try {
+        const response = await axios.get("/api/test");
+        setNumbers(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getNums();
+  }, []);
+
+  return <div>{numbers}</div>;
+}
