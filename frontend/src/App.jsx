bbb4187c9485956fd3af12c14a026c125db3672d
StianSka <stian.skatvedt@gmail.com>
2023-08-03 06:54:32 +0200

    XD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,15 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      halla
-      <Test></Test>
+    <div className="main-page">
+      <div className="nav-bar">topp nav bar</div>
+      <div className="main-container">
+        <div className="side-box">meny ting</div>
+        <div className="main-box">
+          inlegg a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a
+        </div>
+        <div className="side-box">venne liste</div>
+      </div>
     </div>
   );
 }
